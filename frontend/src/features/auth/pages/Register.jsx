8d753db9d393d35f6bae3bc9cb9e6c6ad986bd87
shital barnwal <shitalbarnwal97@gmail.com>
2026-08-17 15:37:36 +0400

    add change

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const Register = () => {
+  const handeSubmit = (e) =>{
+    e.preventDefault();
+}
   return (
-    <div>Register</div>
+    <main>
+    <div className="form-container">
+        <h1>Login</h1>
+
+        <form onSubmit={handeSubmit}>
+            <div className="input-group">
+                <label htmlFor='email'>Email</label>
+                <input type='email' id="email" name='email' placeholder='Enter your email address'  />
+            </div>
+            <div className="input-group">
+                <label htmlFor='password'>Password</label>
+                <input type='password' id="password" name='password' placeholder='Enter your password'  />
+            </div>
+            <button className='button primary-button'>Register</button>
+        </form>
+    </div>
+   </main>
   )
 }
 
